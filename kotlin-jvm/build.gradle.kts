a45582b479d76acbafaaabfec70afdbad4243172
jdiazcano <terkitos@gmail.com>
2020-06-21 17:22:50 +0100

    Better JVM template, now not relying on companion objects!

--- kotlin-jvm/build.gradle.kts
+++ kotlin-jvm/build.gradle.kts
@@ -8,7 +8,7 @@ apply(from = "buildSrc/gradle/repositories.gradle.kts")
 apply(from = "buildSrc/gradle/sourcesets.gradle.kts")
 
 project.ext["mainPackage"] = "com.example"
-project.ext["className"] = "{{name}}"
+project.ext["className"] = "{{name|classname}}"
 
 application {
     mainClassName = "${project.ext["mainPackage"]}.${project.ext["className"]}"
---
+++ kotlin-jvm/src/com/example/{{name|classname}}.kt
@@ -0,0 +1,6 @@
+@file:JvmName("{{name|classname}}")
+package com.example
+
+fun main(args: Array<String>) {
+    println("Hello world")
+}
